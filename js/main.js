3cc333895face60473fff7b93c699d1dbc2086b6
Christian Schmidt <christian@a26z.de>
2020-11-05 21:36:47 +0100

    First upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+import {
+    sendFiles,
+    clickHelper,
+    sendDrop,
+    dropArea
+} from './module.js';
+
+fileElem.addEventListener("change", sendFiles, false); // sendFiles() uploads files
+
+fileSelect.addEventListener("click", clickHelper, false); // helper for click event
+
+dropArea.addEventListener('drop', sendDrop, false); // uploads dropped file
+
+['dragenter', 'dragover', 'dragleave', 'dragexit', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }, false);
+});
+
+dropArea.addEventListener('dragenter', function () {
+    this.classList.add('highlight')
+}, false)
+
+dropArea.addEventListener('dragexit', function () {
+    this.classList.remove('highlight')
+}, false)
---
+++ js/module.js
@@ -0,0 +1,69 @@
+const fileElem = document.getElementById('fileElem'); // change, hidden
+const fileSelect = document.getElementById('fileSelect') // click
+const fileList = document.getElementById('fileList'); // display here
+const progress = document.getElementById('progress-bar');
+
+export const dropArea = document.getElementById('drop-area'); // drop div
+
+export const sendFiles = () => {
+    const files = fileElem.files;
+    uploadAndShow(files);
+};
+
+export const clickHelper = function(e) {
+    progress.value = 0;
+    if (fileElem) {
+        fileElem.click();
+    }
+};
+
+export const sendDrop = e => {
+    progress.value = 0;
+    let dt = e.dataTransfer;
+    let files = dt.files;
+    uploadAndShow(files);
+}
+
+const handleErrors = async (resp) => {
+    let respJson = await resp.json();
+    if(!resp.ok){
+        console.log(resp.statusText, respJson);
+    } else {
+        console.log(respJson);
+        return respJson.secure_url;
+    }
+}
+
+const uploadAndShow = async function(files) {
+    fileList.innerHTML = '';
+    let url = 'https://api.cloudinary.com/v1_1/lucyintheskies/upload';
+    let preset = '---uthjpilr';
+    let pVal = 100 / files.length;
+
+    for (let i = 0; i < files.length; i++) {
+        // upload files
+        let formData = new FormData();
+        formData.append('upload_preset', preset)
+        formData.append('file', files[i]);
+        try {
+            let response = await fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        let imgLink = await handleErrors(response);
+        console.log(imgLink);
+        } catch(err) {
+            console.log('Something went wrong', err);
+        }
+        // create preview
+        const img = document.createElement('img');
+        img.src = URL.createObjectURL(files[i])
+        img.onload = () => {
+            URL.revokeObjectURL(img.src);
+        }
+        img.height = 120;
+        await fileList.appendChild(img);
+        // run progress
+        progress.value += pVal;
+    }
+}
