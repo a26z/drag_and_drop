92c7d59f9e0c9f3019737eff87b2cd3fb204983e
Christian Schmidt <christian@a26z.de>
2020-11-07 00:53:11 +0100

    update

--- js/module.js
+++ js/module.js
@@ -1,3 +1,8 @@
+// import {CDN_PRESET, CDN_URL} from './access.js'
+
+const CDN_URL = null;
+const CDN_PRESET = null;
+
 const fileElem = document.getElementById('fileElem'); // change, hidden
 const fileSelect = document.getElementById('fileSelect') // click
 const fileList = document.getElementById('fileList'); // display here
@@ -26,7 +31,7 @@ export const sendDrop = e => {
 
 const handleErrors = async (resp) => {
     let respJson = await resp.json();
-    if(!resp.ok){
+    if (!resp.ok) {
         console.log(resp.statusText, respJson);
     } else {
         console.log(respJson);
@@ -36,39 +41,39 @@ const handleErrors = async (resp) => {
 
 const uploadAndShow = async function(files) {
     fileList.innerHTML = '';
-    let url = 'https://api.cloudinary.com/v1_1/<your_space_here>/upload';
-    let preset = '<your_upload_preset_here>';
     let pVal = 100 / files.length;
     let figFragment = document.createDocumentFragment();
-
+    let cloudLink = '';
     for (let i = 0; i < files.length; i++) {
         // upload files
         let formData = new FormData();
-        formData.append('upload_preset', preset)
         formData.append('file', files[i]);
-        try {
-            let response = await fetch(url, {
-            method: 'POST',
-            body: formData
-        })
-        let imgLink = await handleErrors(response);
-        console.log(imgLink || 'Link not included in error object.');
-        } catch(err) {
-            console.log('Something went wrong', err);
+        if (CDN_URL && CDN_PRESET) {
+            formData.append('upload_preset', CDN_PRESET)
+            try {
+                let response = await fetch(CDN_URL, {
+                    method: 'POST',
+                    body: formData
+                })
+                cloudLink = await handleErrors(response);
+                console.log(cloudLink || 'Link not included in error object.');
+            } catch (err) {
+                console.log('Something went wrong', err);
+            }
         }
         // generate preview
         let fig = document.createElement('figure');
         let caption = document.createElement('figcaption');
         let img = document.createElement('img');
+        fig.appendChild(img)
+        fig.appendChild(caption)
         img.src = URL.createObjectURL(files[i])
-        caption.innerHTML = `<a href=${img.src} target="_blank">view</a>`;
+        caption.innerHTML = `${files[i].name} <a href=${cloudLink || img.src} target="_blank">view</a>`;
         files[i].onload = () => {
             URL.revokeObjectURL(img.src);
         }
-        fig.appendChild(img)
-        fig.appendChild(caption)
         figFragment.appendChild(fig);
         progress.value += pVal;
     }
-        fileList.appendChild(figFragment);
+    fileList.appendChild(figFragment);
 }
